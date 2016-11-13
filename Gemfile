9997661d72ad691964cfb2cab09c81eaf9e95b29
Christian Maddox <cmaddox5@vols.utk.edu>
2016-11-13 15:13:17 +0000

    Carrierwave is now setup.

--- Gemfile
+++ Gemfile
@@ -51,3 +51,9 @@ gem 'simple_form'
 gem 'devise', '>= 3.2.4'
 
 gem 'factory_girl_rails', '~> 4.0'
+
+gem 'carrierwave', '>= 1.0.0.rc', '< 2.0'
+
+gem 'figaro'
+
+gem 'fog'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161106193040) do
+ActiveRecord::Schema.define(version: 20161113145445) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20161106193040) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "user_id"
+    t.string   "image"
   end
 
   add_index "grams", ["user_id"], name: "index_grams_on_user_id", using: :btree
@@ -38,6 +39,7 @@ ActiveRecord::Schema.define(version: 20161106193040) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "image"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
