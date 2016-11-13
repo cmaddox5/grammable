857765831837c3688d7693f9dc802af93d43a6c4
Christian Maddox <cmaddox5@vols.utk.edu>
2016-11-13 16:13:51 +0000

    Made the app look good. Added a bunch of css changes.

--- app/models/gram.rb
+++ app/models/gram.rb
@@ -1,6 +1,8 @@
 class Gram < ActiveRecord::Base
   validates :message, presence: true
+  validates :image, presence: true
   belongs_to :user
+  has_many :comments
 
   mount_uploader :image, ImageUploader
 end
